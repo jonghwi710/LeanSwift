c9a2874699467d1374a2fde0492ce2191e68a3cc
jonghwi710 <jonghwi1993@icloud.com>
2023-05-01 14:14:59 +0900

    レビュー修正２

--- Contents.swift
+++ Contents.swift
@@ -1,98 +1,50 @@
-// 電子レンジ
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 class MicroewaveMachine {
     // 初期ワット数　500W,1000Wで分ける
     var watt = 0
-    // 500Wの時間(秒)　1000Wの場合は半分の秒数
-    let curryriceTime = 90
-    let chickenTime = 120
-    let eggTime = 60
+   
     
-    let curryriceStartTimeRange500 = 0...90
-    let chickenStartTimeRange500 = 0...120
-    let eggStartTimeRange500 = 0...60
+    var timer: Timer?
+    var count: Int = 0
+    var limit: Int = 10
     
-    let curryriceStartTimeRange1000 = 0...45
-    let chickenStartTimeRange1000 = 0...60
-    let eggStartTimeRange1000 = 0...30
-    
-    enum FoodType {
-        case curryrice
-        case chicken
-        case egg
+    func start() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(countup),
+            userInfo: nil,
+            repeats: true
+        )
     }
     
-    
-    func warmFood(type: FoodType) -> Bool {
-        var isWarm = false
-        switch type {
-        case .curryrice:
-            isWarm = 0 < watt
-            if isWarm {
-                if watt == 500 {
-                    print("500Wでセットされました")
-                    print("\(curryriceTime)秒で調理します。")
-                    for isTimer in (curryriceStartTimeRange500).reversed() {
-                        print("\(isTimer)")
-                    }
-                    print("調理完了しました。")
-                }else if watt == 1000 {
-                    print("1000Wでセットされました")
-                    print("\(curryriceTime / 2)秒で調理します。")
-                    for isTimer in (curryriceStartTimeRange1000).reversed() {
-                        print("\(isTimer)")
-                    }
-                    print("調理完了しました。")
-                }
+    @objc func countup() {
+        count += 1
+        print("\(count)")
+        if watt == 500 {
+            if limit <= count {
+                print("完了しました。")
+                timer?.invalidate()
             }
-        case .chicken:
-            isWarm = 0 < watt
-            if isWarm {
-                if watt == 500 {
-                    print("500Wでセットされました")
-                    print("\(chickenTime)秒で調理します。")
-                    for isTimer in (chickenStartTimeRange500).reversed() {
-                        print("\(isTimer)")
-                    }
-                    print("調理完了しました。")
-                }else if watt == 1000 {
-                    print("1000Wでセットされました")
-                    print("\(chickenTime / 2)秒で調理します。")
-                    for isTimer in (chickenStartTimeRange1000).reversed() {
-                        print("\(isTimer)")
-                    }
-                    print("調理完了しました。")
-                }
+        }else if watt == 1000 {
+            var limit = limit / 2
+            if limit <= count {
+                print("完了しました。")
+                timer?.invalidate()
             }
-        case .egg:
-            isWarm = 0 < watt
-            if isWarm {
-                if watt == 500 {
-                    print("500Wでセットされました")
-                    print("\(eggTime)秒で調理します。")
-                    for isTimer in (eggStartTimeRange500).reversed() {
-                        print("\(isTimer)")
-                    }
-                    print("爆発しました。")
-                }else if watt == 1000 {
-                    print("1000Wでセットされました")
-                    print("\(eggTime / 2)秒で調理します。")
-                    for isTimer in (eggStartTimeRange1000).reversed() {
-                        print("\(isTimer)")
-                    }
-                    print("爆発しました。")
-                }
-            }
-            
         }
-        return isWarm
     }
+    
 }
 
-let warmMachine = MicroewaveMachine()
-warmMachine.watt = 500
+let microewaveMachine = MicroewaveMachine()
+microewaveMachine.watt = 1000
+microewaveMachine.start()
 
-let isWarm = warmMachine.warmFood(type: .egg)
 
 
 
